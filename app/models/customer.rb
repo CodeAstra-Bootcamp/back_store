6869271bb4976456672560f7eac6557b2243c68e
B V Satyaram <bvsatyaram@gmail.com>
2016-03-23 19:17:55 +0530

    Add rake task for mocking placement of an order

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -14,4 +14,5 @@
 #
 
 class Customer < ActiveRecord::Base
+  has_many :orders
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -10,4 +10,5 @@
 
 class Order < ActiveRecord::Base
   belongs_to :customer
+  has_many :items, class_name: OrderItem.name
 end
