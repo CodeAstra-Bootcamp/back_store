0995a86bf81c5a878e16c411899d49702b59b43c
B V Satyaram <bvsatyaram@gmail.com>
2016-03-23 18:15:03 +0530

    Add customer seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'rubygems'
+require 'faker'
+
+def dot
+  print '.'
+end
+
+def seed_customer(i)
+  Customer.find_or_create_by(email: "customer_#{i}@backstore.com") do |customer|
+    customer.name = Faker::Name.name
+    customer.phone = Faker::PhoneNumber.phone_number
+    customer.address = "#{Faker::Address.street_address}, #{Faker::Address.city}, #{Faker::Address.postcode}"
+  end
+  dot
+end
+
+puts "# Seeding Customers"
+(1..100).each do |i|
+  seed_customer(i)
+end
+puts "  Done."
